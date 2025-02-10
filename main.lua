7252c453212f6196651bd43a04eb4601a9ac9f5f
ImDamc <damcproseeee@outlook.com>
2025-02-10 14:21:56 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -31,7 +31,7 @@ local playersService = cloneref(game:GetService('Players'))
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/ImDamc/VapeV4Reborn'..'/'..select(1, path:gsub('newvape/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/ImDamc/VapeV4Reborn/refs/heads/main/'..'/'..select(1, path:gsub('newvape/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
