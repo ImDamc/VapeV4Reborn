c53dd0f065482f6796337750a58e4f5eac221ddf
ImDamc <damcproseeee@outlook.com>
2025-02-10 14:29:47 -0500

    Update new.lua

--- guis/new.lua
+++ guis/new.lua
@@ -312,7 +312,7 @@ end
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/ImDamc/VapeV4Reborn/refs/heads/main/'..'/'..select(1, path:gsub('newvape/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/ImDamc/VapeV4Reborn/refs/heads/main/'..select(1, path:gsub('newvape/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
